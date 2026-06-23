13381e888eb427551aa023d2ffec944165bc5aa6
Lavanya <lavanjhb025@student.wethinkcode.co.za>
2026-06-23 11:49:36 +0200

    feat: added custom regex patterns wrapped by PatternRecognizer and injected them into the analyzer. #12 #13 #14

--- app/services/masking.py
+++ app/services/masking.py
@@ -1,5 +1,5 @@
 from typing import Dict, Any
-from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer import AnalyzerEngine, PatternRecognizer, Pattern, RecognizerRegistry
 from presidio_anonymizer import AnonymizerEngine
 
 class PrivacyMaskingService:
@@ -10,6 +10,38 @@ class PrivacyMaskingService:
         """
         self.analyzer = AnalyzerEngine()
         self.anonymizer = AnonymizerEngine()
+
+    def south_african_pii_recognizer(self):
+        """
+        Method that adds custom regex rules for South African PII like ID numbers,
+        passport numbers, South African mobile numbers, vehicle registrations, and SARS UIF
+        numbers
+
+        """
+        sa_patterns = [
+            PatternRecognizer(
+                supported_entity="ZA_ID_NUM",
+                patterns=[Pattern(name="ZA_ID_NUM",regex=r"^\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])\d{4}[01][89]\d$",score=0.85)]
+                ),
+            PatternRecognizer(
+                supported_entity="ZA_PASSPORT_NUM",
+                patterns=[Pattern(name="ZA_PASSPORT_NUM", regex=r"^[ADMTadmt]\d{8}$", score=0.7)]
+            ),
+            PatternRecognizer(
+                supported_entity="ZA_PHONE_NUM",
+                patterns=[Pattern(name="ZA_PHONE_NUM", regex=r"^(\+27|0)[6-8][0-9]{8}$", score=0.85)]
+            ),
+            PatternRecognizer(
+                supported_entity="ZA_VEHICLE_REG",
+                patterns=[Pattern(name="ZA_VEHICLE_REG", regex=r"^(?:[B-DF-HJ-NP-TV-Z]{2}\s?\d{2}\s?[B-DF-HJ-NP-TV-Z]{2}(?:\s?GP)?|[A-Z]{3}\s?\d{3}\s?(?:EC|FS|L|MP|NC|NW|WP)|[A-Z]{1,3}\s?\d{1,6}(?:\s?ZN)?|[A-Z]{2}\s?\d{2}\s?[A-Z]{2}\s?ZN|[A-Z0-9]{1,7}\s?(?:GP|EC|FS|L|MP|NC|NW|WP|ZN))$", score=0.65)]
+            ),
+            PatternRecognizer(
+                supported_entity="ZA_UIF_NUM",
+                patterns=[Pattern(name="ZA_UIF_NUM", regex=r"^U\d{9}$", score=0.8)]
+            )
+            ]
+        for pattern in sa_patterns:
+            self.analyzer.registry.add_recognizer(pattern)
 
     def mask_text(self, text: str) -> Dict[str, Any]:
         """
@@ -33,3 +65,5 @@ class PrivacyMaskingService:
             "anonymized_text": anonymized_result.text,
             "entities_masked_count": len(analysis_results)
         }
+    
+    
