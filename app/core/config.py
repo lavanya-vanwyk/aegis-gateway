b897b763d6e7f9598467f8c70d9a32065faa8e00
Lavanya <lavanjhb025@student.wethinkcode.co.za>
2026-06-29 11:31:30 +0200

    feat: add fernet key to .env to be used to encrypt PII payloads before they are stored in the Redis Vault and to decrypt before mapping and rehydration. retrieve_mapping function will not return encrypted text. #29

--- app/core/config.py
+++ app/core/config.py
@@ -3,13 +3,13 @@ from pydantic import SecretStr
 
 
 class Settings(BaseSettings):
-    # Pydantic looks for an environment variable named APP_ENV or target file
     PROJECT_NAME: str = "Aegis Gateway"
     ENVIRONMENT: str = "dev"
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     # SecretStr automatically masks sensitive values when printing/logging
     LLM_API_KEY: SecretStr
+    VAULT_ENCRYPTION_KEY: str
     # Configuration block for Pydantic Settings
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
--- app/services/storage.py
+++ app/services/storage.py
@@ -1,10 +1,18 @@
 from app.core.database import redis_manager
+from app.core.config import settings
+from cryptography.fernet import Fernet
 
 
 class TokenVaultService:
     def __init__(self):
         # 5 minutes
         self.ttl_seconds = 300
+        if not getattr(settings, "VAULT_ENCRYPTION_KEY", None):
+            raise ValueError(
+                "Critical Security Error: VAULT_ENCRYPTION_KEY is missing."
+            )
+
+        self.cipher = Fernet(settings.VAULT_ENCRYPTION_KEY)
 
     async def store_mapping(self, token_id: str, original_text: str) -> None:
         """
@@ -15,7 +23,9 @@ class TokenVaultService:
         if not client:
             raise RuntimeError("Redis client pool is not initialized.")
 
-        await client.set(name=token_id, value=original_text, ex=self.ttl_seconds)
+        encrypted_payload = self.cipher.encrypt(original_text.encode("utf-8"))
+
+        await client.set(name=token_id, value=encrypted_payload, ex=self.ttl_seconds)
 
     async def retrieve_mapping(self, token_id: str) -> str | None:
         """
@@ -25,6 +35,10 @@ class TokenVaultService:
         if not client:
             raise RuntimeError("Redis client pool is not initialized.")
 
-        original_text = await client.getdel(name=token_id)
+        encrypted_payload = await client.getdel(name=token_id)
 
-        return original_text
+        if encrypted_payload:
+            decrypted_text = self.cipher.decrypt(encrypted_payload).decode("utf-8")
+            return decrypted_text
+
+        return None
